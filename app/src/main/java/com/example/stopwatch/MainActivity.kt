1ada54e5a932c43515b4ece82b74e6c9a37945ca
smoljankin <smoljankin@gmail.com>
2024-01-22 11:08:33 +0200

    Created variables to access widgets

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,8 +2,20 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var timerText: TextView
+    private lateinit var startButton: Button
+    private lateinit var stopButton: Button
+    private lateinit var resetButton: Button
+    private lateinit var storeButton: Button
+    private lateinit var storedItems: ListView
+
+    private val storedItemsData: MutableList<String> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
