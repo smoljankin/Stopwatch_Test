3e800fefabe338f4716e294b3a875f13ae204179
smoljankin <smoljankin@gmail.com>
2024-03-12 16:26:02 +0200

    Add test startButton and start_stop_Buttom

--- app/src/androidTest/java/com/example/stopwatch/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/stopwatch/ExampleInstrumentedTest.kt
@@ -8,6 +8,25 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
+
+import androidx.test.espresso.matcher.ViewMatchers.withId
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
+import androidx.test.espresso.action.ViewActions.click
+
+import androidx.test.filters.LargeTest
+
+import org.junit.Rule
+
+
+
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -15,10 +34,38 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityScenarioRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.stopwatch", appContext.packageName)
+    }
+
+    @Test
+    fun startButton_Click() {
+
+        val scenario = activityScenarioRule.scenario
+
+        // Виконуємо дії з UI
+        onView(withId(R.id.startButton)).perform(click())
+
+    }
+
+    @Test
+    fun start_stop_Button_Click() {
+
+        val scenario = activityScenarioRule.scenario
+
+        // Виконуємо дії з UI
+        onView(withId(R.id.startButton)).perform(click())
+        Thread.sleep(5000);
+        onView(withId(R.id.stopButton)).perform(click())
+
+
     }
 }
