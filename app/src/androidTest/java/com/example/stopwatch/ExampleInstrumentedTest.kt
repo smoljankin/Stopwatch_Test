efc6006536c411820b6b4836c6a18233e44d11ef
smoljankin <smoljankin@gmail.com>
2024-03-27 13:30:06 +0200

    Editing test functions

--- app/src/androidTest/java/com/example/stopwatch/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/stopwatch/ExampleInstrumentedTest.kt
@@ -1,5 +1,6 @@
 package com.example.stopwatch
 
+import android.os.Handler
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -10,21 +11,31 @@ import org.junit.Assert.*
 
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
+//import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.*
 
 import androidx.test.espresso.matcher.ViewMatchers.withId
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.idling.CountingIdlingResource
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.isNotEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withText
 
 import androidx.test.filters.LargeTest
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.not
 
 import org.junit.Rule
-
-
+import java.util.concurrent.TimeUnit
 
 
 /**
@@ -48,24 +59,54 @@ class ExampleInstrumentedTest {
 
     @Test
     fun startButton_Click() {
-
         val scenario = activityScenarioRule.scenario
 
-        // Виконуємо дії з UI
         onView(withId(R.id.startButton)).perform(click())
-
     }
 
     @Test
-    fun start_stop_Button_Click() {
-
+    fun timer_Text() {
+        // Отримуємо посилання на активність через сценарій
         val scenario = activityScenarioRule.scenario
 
-        // Виконуємо дії з UI
-        onView(withId(R.id.startButton)).perform(click())
-        Thread.sleep(5000);
-        onView(withId(R.id.stopButton)).perform(click())
+        //перевірка чи  текст  "00:00:00.00"
 
-
+        onView(withId(R.id.timerText)).check(matches(withText("00:00:00.00")))
     }
+    @Test
+    fun testStartButtonActivation() {
+        // Перевірка, що кнопка "Start" активна перед кліком
+        onView(withId(R.id.startButton))
+            .check(matches(isEnabled()))
+
+        // Клік на кнопку "Start"
+        onView(withId(R.id.startButton))
+            .perform(click())
+
+        // Перевірка, що кнопка "Start" стала неактивною після кліку
+        onView(withId(R.id.startButton))
+            .check(matches(not(isEnabled())))
+
+        // Перевірка, що кнопка "Stop" стала активною після кліку
+        onView(withId(R.id.stopButton))
+            .check(matches(isEnabled()))
+    }
+
+    @Test
+    fun testTimerButton() {
+        val scenario = activityScenarioRule.scenario
+
+       onView(withId(R.id.startButton)).perform(click())
+
+       Thread.sleep(5000)
+
+        onView(withId(R.id.stopButton)).perform(click())
+        onView(withId(R.id.timerText)).check(matches(withText("00:00:00.00"))) // Assuming the timer shows "1" after 1 second
+    }
+
+
+
+
+
 }
+
